984053f73b41f50645be220c5badf64976c060c8
D4rl1nG <quangaobn@gmail.com>
2022-08-15 21:40:52 +0700

    Add notify realtime

--- src/templates/DashBoardTemplate/DashBoard.js
+++ src/templates/DashBoardTemplate/DashBoard.js
@@ -1,10 +1,31 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../../components/Header/Header';
 import Navigation from '../../components/Navigation/Navigation';
 import PhoneView from '../../components/PhoneView/PhoneView';
+import SockJS from 'sockjs-client'
+import {over} from 'stompjs';
+import { openNotification } from '../../utils/Notification';
+
+let stompClient = null;
 
 export const DashBoard = (props) => {
     const { ComponentRender, route } = props
+
+    useEffect(()=>{
+        connect(localStorage.getItem('username'))
+    },[])
+
+    const  connect = (username) => {
+        let socket = new SockJS('http://localhost:8080/notification');
+        console.log("connect to server");
+        stompClient = over(socket);
+        stompClient.connect({}, function() {
+           console.log('Web Socket is connected');
+           stompClient.subscribe('/queue/notification/' + username,(message)=>{
+            openNotification('info', message.body)
+           });
+        });
+     }
 
     return <>
         <div className='-z-1 relative' style={{ backgroundColor: 'rgba(0,0,0,.025)', minHeight: '100vh' }}>
